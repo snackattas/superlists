520234c04828a41f2e3cf2a44c83388500d7a424
snackattas <zach.attas@gmail.com>
2016-12-22 15:08:10 -0600

    Use bootstrap to improve layout

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import unittest
 
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome('/home/zach/Desktop/summary/chromedriver')
         self.browser.implicitly_wait(3)
