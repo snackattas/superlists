d46edee28443bc0e608b410f3801b5a8dfb80729
snackattas <zach.attas@gmail.com>
2016-12-25 09:43:03 -0600

    restructuring of files

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,17 +1,16 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-import unittest
 import sys
 
-
 class NewVisitorTest(StaticLiveServerTestCase):
+
     @classmethod
     def setUpClass(cls):
         for arg in sys.argv:
             if 'liveserver' in arg:
                 cls.server_url = 'http://' + arg.split('=')[1]
+                cls.live_server_url = ''
                 return
         super().setUpClass()
         cls.server_url = cls.live_server_url
@@ -21,12 +20,13 @@ class NewVisitorTest(StaticLiveServerTestCase):
         if cls.server_url == cls.live_server_url:
             super().tearDownClass()
 
-
     def setUp(self):
         self.browser = webdriver.Chrome('/home/zach/Desktop/summary/chromedriver')
         self.browser.implicitly_wait(3)
+
     def tearDown(self):
         self.browser.quit()
+
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
@@ -34,7 +34,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.server_url)
@@ -48,13 +48,14 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            'Enter a to-do item')
+            'Enter a to-do item'
+        )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
-            # When she hits enter, the page updates, and now the page lists
+        # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -67,9 +68,11 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        # [...rest of comments as before]
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # Satisfied, she goes back to sleep
+
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith start a new todo list
@@ -82,14 +85,13 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # She notices that her list has a unique URL
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
+
         # Now a new user, Francis, comes along to the site.
 
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
         self.browser.quit()
         self.browser = webdriver.Chrome('/home/zach/Desktop/summary/chromedriver')
-        self.browser.implicitly_wait(3)
-
 
         # Francis visits the home page.  There is no sign of Edith's
         # list
@@ -115,6 +117,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+        # Satisfied, they both go back to sleep
+
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
@@ -126,11 +130,15 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=8)
+            delta=10
+        )
 
+        # She starts a new list and sees the input is nicely
+        # centered there too
         inputbox.send_keys('testing\n')
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=8)
+            delta=10
+)
