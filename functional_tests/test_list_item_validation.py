f03bfcc85d8c16652f6d7fcf005cd5427e357ef1
snackattas <zach.attas@gmail.com>
2016-12-29 17:30:48 -0600

    first cut of FT for disappearing error message after bad then good input

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,6 +3,9 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
@@ -43,3 +46,21 @@ class ItemValidationTest(FunctionalTest):
         self.check_for_row_in_list_table('1: Buy wellies')
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You've already got this in your list")
+
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith starts a list and causes a validation error:
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Banter too thick\n')
+        self.check_for_row_in_list_table('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick\n')
+
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        # She is pleased to see that the error message disappears
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
