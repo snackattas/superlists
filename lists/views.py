3e729ac4dbb82b8e7afee9e646ec7cf504a4fae5
snackattas <zach.attas@gmail.com>
2016-12-29 16:51:03 -0600

    duplicate item in list is now an error!

--- lists/views.py
+++ lists/views.py
@@ -1,29 +1,30 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.forms import ItemForm
-from lists.models import Item, List
 from django.core.exceptions import ValidationError
-import logging
+
+from lists.forms import ExistingListItemForm, ItemForm
+from lists.models import Item, List
+
 
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    form = ItemForm()
-    if request.method == 'POST':
-        form = ItemForm(data=request.POST)
-        if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
-            return redirect(list_)
-    return render(request, 'list.html', {'list': list_, "form": form})
-
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    form = ExistingListItemForm(for_list=list_)
+    if request.method == 'POST':
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(list_)
+    return render(request, 'list.html', {'list': list_, "form": form})
