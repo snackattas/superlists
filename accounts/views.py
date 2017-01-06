56b3897c7f3c21790b0e83691e657f300ea1697a
snackattas <zach.attas@gmail.com>
2017-01-06 11:39:06 -0600

    rough mock up and running

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import uuid
+import sys
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.shortcuts import redirect, render
+from django.core.mail import send_mail
 
-# Create your views here.
+from accounts.models import Token
+import logging
+
+def send_login_email(request):
+    email = request.POST['email']
+    send_mail(
+        'Your login link for Superlists',
+        'body text tbc',
+        'noreply@superlists',
+        [email])
+    return redirect('/')
--- superlists/settings.py
+++ superlists/settings.py
@@ -36,8 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'lists',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -119,3 +123,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
