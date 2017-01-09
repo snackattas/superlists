bd1e988cf86fc33b2fd1e21cdd02430bdd3e2c67
snackattas <zach.attas@gmail.com>
2017-01-09 10:21:16 -0600

    test_my_lists: precreate sessions, move login checks into base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -14,6 +14,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         cls.chromedriver_path = DIR
         return DIR
 
+
     @classmethod
     def setUpClass(cls):
         cls.setChromedriverPath()
@@ -25,22 +26,39 @@ class FunctionalTest(StaticLiveServerTestCase):
         super().setUpClass()
         cls.server_url = cls.live_server_url
 
+
     @classmethod
     def tearDownClass(cls):
         if cls.server_url == cls.live_server_url:
             super().tearDownClass()
 
+
     def setUp(self):
         self.browser = webdriver.Chrome(executable_path=self.chromedriver_path)
         self.browser.implicitly_wait(3)
 
+
     def tearDown(self):
         self.browser.quit()
+
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
+
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
+
+
+    def assert_logged_in(self, email):
+        self.browser.find_element_by_link_text('Log out')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(email, navbar.text)
+
+
+    def assert_logged_out(self, email):
+        self.browser.find_element_by_name('email')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
