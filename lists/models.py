fe3945fb197c13e33c788b324b544e026155e75a
snackattas <zach.attas@gmail.com>
2017-01-10 17:10:22 -0600

    lists can have owners, which are saved on creation

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.core.urlresolvers import reverse
-
+from django.conf import settings
 
 class List(models.Model):
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
 
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
@@ -11,13 +13,13 @@ class List(models.Model):
         return str(self.id)
 
 class Item(models.Model):
+
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
 
     class Meta:
         ordering = ('id',)
         unique_together = ('list', 'text')
-
 
     def __str__(self):
         return self.text
--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class ItemModelTest(TestCase):
 
@@ -51,6 +53,17 @@ class ItemModelTest(TestCase):
             [item1, item2, item3])
 
 class ListModelTest(TestCase):
+
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
+
+
+    def test_lists_can_have_owners(self):
+        user = User.objects.create(email='a@b.com')
+        list_ = List.objects.create(owner=user)
+        self.assertIn(list_, user.list_set.all())
+
+
+    def test_list_owner_is_optional(self):
+        List.objects.create()  # should not raise
