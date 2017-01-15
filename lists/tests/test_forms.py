ebcfd0c94e8799232a2904f28ae21ff425c75fa4
snackattas <zach.attas@gmail.com>
2017-01-15 13:12:08 -0600

    adding readme

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,8 +1,63 @@
 from django.test import TestCase
 from lists.forms import (
     DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
-    ExistingListItemForm, ItemForm)
+    ExistingListItemForm, ItemForm, NewListForm)
 from lists.models import Item, List
+import unittest
+from unittest.mock import patch, Mock
+
+class NewListFormTest(unittest.TestCase):
+
+    # @patch('lists.forms.List')
+    # @patch('lists.forms.Item')
+    # def test_save_creates_new_list_and_item_from_post_data(
+    #     self, mockItem, mockList
+    # ):
+    #     mock_item = mockItem.return_value
+    #     mock_list = mockList.return_value
+    #     user = Mock()
+    #     form = NewListForm(data={'text': 'new item text'})
+    #     form.is_valid()
+    #
+    #     def check_item_text_and_list():
+    #         self.assertEqual(mock_item.text, 'new item text')
+    #         self.assertEqual(mock_item.list, mock_list)
+    #         self.assertTrue(mock_list.save.called)
+    #     mock_item.save.side_effect = check_item_text_and_list
+    #
+    #     form.save(owner=user)
+    #
+    #     self.assertTrue(mock_item.save.called)
+
+    @patch('lists.forms.List.create_new')
+    def test_save_creates_new_list_from_post_data_if_user_not_authenticated(
+        self, mock_List_create_new
+    ):
+        user = Mock(is_authenticated=False)
+        form = NewListForm(data={'text': 'new item text'})
+        form.is_valid()
+        form.save(owner=user)
+        mock_List_create_new.assert_called_once_with(
+            first_item_text='new item text')
+
+    @patch('lists.forms.List.create_new')
+    def test_save_creates_new_list_with_owner_if_user_authenticated(
+        self, mock_List_create_new
+    ):
+        user = Mock(is_authenticated=True)
+        form = NewListForm(data={'text': 'new item text'})
+        form.is_valid()
+        form.save(owner=user)
+        mock_List_create_new.assert_called_once_with(
+            first_item_text='new item text', owner=user)
+
+    @patch('lists.forms.List.create_new')
+    def test_save_returns_new_list_object(self, mock_List_create_new):
+        user = Mock(is_authenticated=True)
+        form = NewListForm(data={'text': 'new item text'})
+        form.is_valid()
+        response = form.save(owner=user)
+        self.assertEqual(response, mock_List_create_new.return_value)
 
 class ItemFormTest(TestCase):
 
@@ -16,15 +71,6 @@ class ItemFormTest(TestCase):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'],[EMPTY_ITEM_ERROR])
-
-
-    def test_form_save_handles_saving_to_a_list(self):
-        list_ = List.objects.create()
-        form = ItemForm(data={'text': 'do me'})
-        new_item = form.save(for_list=list_)
-        self.assertEqual(new_item, Item.objects.first())
-        self.assertEqual(new_item.text, 'do me')
-        self.assertEqual(new_item.list, list_)
 
 
 class ExistingListItemFormTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 
-from lists.forms import ExistingListItemForm, ItemForm
+from lists.forms import ExistingListItemForm, ItemForm, NewListForm
 from lists.models import Item, List
 
 User = get_user_model()
@@ -12,16 +12,24 @@ User = get_user_model()
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
+# def new_list(request):
+#     form = ItemForm(data=request.POST)
+#     if form.is_valid():
+#         list_ = List()
+#         if request.user.is_authenticated:
+#             list_.owner = request.user
+#         list_.save()
+#         form.save(for_list=list_)
+#         return redirect(list_)
+#     else:
+#         return render(request, 'home.html', {"form": form})
+
 def new_list(request):
-    form = ItemForm(data=request.POST)
+    form = NewListForm(data=request.POST)
     if form.is_valid():
-        list_ = List()
-        list_.owner = request.user
-        list_.save()
-        form.save(for_list=list_)
+        list_ = form.save(owner=request.user)
         return redirect(list_)
-    else:
-        return render(request, 'home.html', {"form": form})
+    return render(request, 'home.html', {'form': form})
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
