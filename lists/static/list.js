f5c89433b5febe760dcaa0ac55c1c167e5c607e7
snackattas <zach.attas@gmail.com>
2017-01-20 11:35:09 -0600

    raw ajax complete, with tests! Although I did copy the code straight from harry's github...

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,7 +1,37 @@
 window.Superlists = {};
+
+window.Superlists.updateItems = function (url) {
+  $.get(url).done(function (response) {
+    var rows = '';
+    for (var i=0; i<response.length; i++) {
+      var item = response[i];
+      rows += '\n<tr><td>' + (i+1) + ': ' + item.text + '</td></tr>';
+    }
+    $('#id_list_table').html(rows);
+  });
+};
+
 window.Superlists.initialize = function (url) {
   $('input[name="text"]').on('keypress', function () {
     $('.has-error').hide();
   });
-  $.get(url);
+
+  if (url) {
+    window.Superlists.updateItems(url);
+
+    var form = $('#id_item_form');
+    form.on('submit', function(event) {
+      event.preventDefault();
+      $.post(url, {
+        'text': form.find('input[name="text"]').val(),
+        'csrfmiddlewaretoken': form.find('input[name="csrfmiddlewaretoken"]').val(),
+      }).done(function () {
+        $('.has-error').hide();
+        window.Superlists.updateItems(url);
+      }).fail(function (xhr) {
+        $('.has-error').show();
+        $('.has-error .help-block').text(xhr.responseJSON.error);
+      });
+    });
+  }
 };
